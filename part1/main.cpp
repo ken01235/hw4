1681fc86030daef238d72314ae183ad2192d4a5c
ken01235 <kenkenrc08@gmail.com>
2021-06-22 12:38:29 -0700

    .

--- part1/main.cpp
+++ part1/main.cpp
@@ -1,41 +1,43 @@
-#include "mbed.h"
+#include"mbed.h"
 #include "bbcar.h"
 
+BufferedSerial pc(USBTX,USBRX); //tx,rx
+BufferedSerial uart(D1,D0); //tx,rx
 Ticker servo_ticker;
-Ticker encoder_ticker;
 PwmOut pin5(D5), pin6(D6);
-DigitalIn encoder(D11);
-volatile int steps;
-volatile int last;
-
 BBCar car(pin5, pin6, servo_ticker);
-void encoder_control() {
-   int value = encoder;
-   if (!last && value) steps++;
-   last = value;
-}
 
-int main()
-{
-    double pwm_table0[] = {-150, -120, -90, -60, -30, 0, 30, 60, 90, 120, 150};
-    double speed_table0[] = {-15.785, -15.307, -14.190, -11.002, -4.863, 0.000, 6.617, 11.878, 14.589, 15.705, 16.104};
-    double pwm_table1[] = {-150, -120, -90, -60, -30, 0, 30, 60, 90, 120, 150};
-    double speed_table1[] = {-22.640, -23.598, -24.315, -24.395, -11.081, 0.000, 15.705, 23.199, 22.960, 23.359, 22.003};
+int pid = 0;
+int i = 0;
+bool m = false;
 
-    car.setCalibTable(11, pwm_table0, speed_table0, 11, pwm_table1, speed_table1);
+int main(){
+   uart.set_baud(9600);
+   while(1){
+      
+      if(uart.readable()){
+         char recv[1];
+         uart.read(recv, sizeof(recv));
+         //pc.write(recv, sizeof(recv));
+         //
 
-    car.goStraightCalib(15);
-    encoder_ticker.attach(&encoder_control, 10ms);
-    steps = 0;
-    last = 0;
-    while(steps*6.5*3.14/32 < 30) {
-        printf("encoder = %d\r\n", steps); 
-        ThisThread::sleep_for(100ms);
-    }
-    car.turn(15, -1);
-    while(steps*6.5*3.14/32 < 9) {
-        ThisThread::sleep_for(100ms);
-    }
-    car.stop();
-    return 0;
+         if (recv[0] != '\n'){
+            pid = pid * 10 + atoi(recv);
+            //printf("S = %d\n", atoi(recv));
+            //printf("%d\n", pid);
+         } else {
+            //printf("\n");
+            printf("%d\n", pid);
+            i = pid;
+            pid = 0;
+            if (i > 10){ //+l-r
+               car.turn(40, 0.4);//r
+            } else if (i > 2){
+               car.turn(40, -0.4);//l
+            } else {
+               car.goStraight(40);
+            }
+         }
+      }
+   }
 }
--- part2/main.cpp
+++ part2/main.cpp
@@ -1,21 +1,28 @@
-#include"mbed.h"
+#include "mbed.h"
+#include "bbcar.h"
+#include "bbcar_rpc.h"
 
-BufferedSerial pc(USBTX,USBRX);
-BufferedSerial uart(D1,D0);
+Ticker servo_ticker;
+PwmOut pin5(D5), pin6(D6);
+BufferedSerial pc(USBTX, USBRX);
 
-int main()
-{
-    uart.set_baud(9600);
-    while(1){
-        if(uart.readable()){
-            char recv[1];
-            uart.read(recv, sizeof(recv));
-            string text;
-            text = pc.write(recv, sizeof(recv));
-            for (int i = 0; i < sizeof(recv); ++i) {
-                //printf("%c", text[i]);
-            }
-        }
-    }
-    
+BBCar car(pin5, pin6, servo_ticker);
+
+int main() {
+   char buf[256], outbuf[256];
+   FILE *devin = fdopen(&pc, "r");
+   FILE *devout = fdopen(&pc, "w");
+   while (1) {
+      memset(buf, 0, 256);
+      for( int i = 0; ; i++ ) {
+         char recv = fgetc(devin);
+         if(recv == '\n') {
+            printf("\r\n");
+            break;
+         }
+         buf[i] = fputc(recv, devout);
+      }
+      RPC::call(buf, outbuf);
+      printf("%s\r\n", outbuf);
+   }
 }
